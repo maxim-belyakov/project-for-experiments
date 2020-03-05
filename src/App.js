b9f3310f31f3677851c888bb92d54a54afbee359
Maxim Belyakov <maxim.a.belyakov+g@gmail.com>
2020-03-05 19:15:21 +0200

    state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-/*
-Let's practice props and mapping components on our todo list app!
-
-I've created a js file with some todos data in it, which I'm imported into this file. (Normally this data would come from an API call, not a local file). 
-
-Challenge: Using the array map method, render a child component for each todo item in the todosData array and pass the relevant data to it.
-*/
-
 import React from "react"
-import TodoItem from "./TodoItem"
-import todosData from "./todosData"
 
-import './App.css'
+// Given a stateless functional component, add state to it
+// state should have a property called `isLoggedIn` which is a boolean
+// (true if logged in, false if not)
+// Then, give your best shot at rendering the word "in" if the user is logged in
+// or "out" if the user is logged out.
 
-function App() {
-  const todosComponents = todosData.map(item => <TodoItem key={item.id} text={item.text} completed={item.completed} />)
-
-  return (
-    <div className="todo-list">
-      {todosComponents}
-    </div>
-  )
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            isLoggedIn: true
+        }
+    }
+    
+    render() {
+        let wordDisplay
+        if (this.state.isLoggedIn === true) {
+            wordDisplay = "in"
+        } else {
+            wordDisplay = "out"
+        }
+        return (
+            <div>
+                <h1>You are currently logged {wordDisplay}</h1>
+            </div>
+        )
+    }
 }
 
 export default App
