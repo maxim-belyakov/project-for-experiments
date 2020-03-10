59a669ae99159c0fcc18366079f4e2d6c4f2f421
Maxim Belyakov <maxim.a.belyakov+g@gmail.com>
2020-03-10 21:09:40 +0200

    how I want to fix

--- src/App.js
+++ src/App.js
@@ -1,29 +1,53 @@
+/**
+ * Let's make it so our checkbox can actually mark our todo as complete or incomplete!
+ * This challenge is a little more involved than some of the past ones. Check the comments 
+ * in the code for some help on accomplishing this one
+ * 
+ * Challenge: 
+ * 1. Create an event handler in the App component for when the checkbox is clicked (which is an `onChange` event)
+ *    a. This method will be the trickest part. Check the comments in the stubbed-out method below for some pseudocode to help guide you through this part
+ * 2. Pass the method down to the TodoItem component
+ * 3. In the TodoItem component, make it so when the `onChange` event happens, it calls the `handleChange` method and passes the id of the todo into the function
+ */
+
 import React from "react"
+import TodoItem from "./TodoItem"
+import todosData from "./todosData"
 
 class App extends React.Component {
     constructor() {
         super()
         this.state = {
-            count: 0
+            todos: todosData
         }
-        this.handleClick = this.handleClick.bind(this)
+        this.handleChange = this.handleChange.bind(this)
     }
     
-    handleClick() {
+    handleChange(id) {
         this.setState(prevState => {
-            return {
-                count: prevState.count + 1
-            }
+            // const updatedTodos = prevState.todos.map(todo => {
+            //     if (todo.id === id) {
+            //         todo.completed = !todo.completed
+            //     }
+            //     return todo
+            // })
+            const updatedTodos = prevState.todos
+            updatedTodos[id - 1].completed = !prevState.todos[id - 1].completed
+            // todos[id - 1].completed = !prevState.todos[id].completed
+            return { todos: updatedTodos }
         })
+        console.log(id)
+        console.log(this.state.todos[id-1])
     }
     
     render() {
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
+        
         return (
-            <div>
-                <h1>{this.state.count}</h1>
-                <button onClick={this.handleClick}>Change!</button>
+            <div className="todo-list">
+                {todoItems}
             </div>
-        )
+        )    
     }
 }
 
