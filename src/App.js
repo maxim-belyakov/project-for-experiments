093fe50fd598936411703569caafd9fceb6b554e
Maxim Belyakov <maxim.a.belyakov+g@gmail.com>
2020-03-13 14:12:14 +0200

    ERROR; didUpdate limit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // https://scrimba.com/g/greacthooks
 
 import React from "react"
+import randomcolor from "randomcolor"
 
 class App extends React.Component {
     constructor() {
         super()
         this.state = {
-            count: 0
+            count: 0,
+            color: ""
         }
         this.increment = this.increment.bind(this)
     }
@@ -19,19 +21,15 @@ class App extends React.Component {
         })
     }
     
-    componentDidMount() {
-        console.log("Mounted")
-    }
-    
     componentDidUpdate() {
-        console.log("Did Update")
+        const newColor = randomcolor()
+        this.setState({color: newColor})
     }
     
     render() {
-        console.log("Render")
         return (
             <div>
-                <h1>{this.state.count}</h1>
+                <h1 style={{color: this.state.color}}>{this.state.count}</h1>
                 <button onClick={this.increment}>
                     Increment!
                 </button>
