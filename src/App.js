a49fe9ece8e5c4afc10198fb7b8bd4edeb586328
Maxim Belyakov <maxim.a.belyakov+g@gmail.com>
2020-03-13 14:07:56 +0200

    render, didMount, didUpdate

--- src/App.js
+++ src/App.js
@@ -1,55 +1,40 @@
-/**
- * Let's make it so our checkbox can actually mark our todo as complete or incomplete!
- * This challenge is a little more involved than some of the past ones. Check the comments 
- * in the code for some help on accomplishing this one
- * 
- * Challenge: 
- * 1. Create an event handler in the App component for when the checkbox is clicked (which is an `onChange` event)
- *    a. This method will be the trickest part. Check the comments in the stubbed-out method below for some pseudocode to help guide you through this part
- * 2. Pass the method down to the TodoItem component
- * 3. In the TodoItem component, make it so when the `onChange` event happens, it calls the `handleChange` method and passes the id of the todo into the function
- */
+// https://scrimba.com/g/greacthooks
 
 import React from "react"
-import TodoItem from "./TodoItem"
-import todosData from "./todosData"
-
-import "./App.css"
 
 class App extends React.Component {
     constructor() {
         super()
         this.state = {
-            todos: todosData
+            count: 0
         }
-        this.handleChange = this.handleChange.bind(this)
+        this.increment = this.increment.bind(this)
     }
     
-    handleChange(id) {
+    increment() {
         this.setState(prevState => {
-            const updatedTodos = prevState.todos.map(todo => {
-                if (todo.id === id) {
-                    return {
-                        ...todo,
-                        completed: !todo.completed
-                    }
-                }
-                return todo
-            })
-            console.log(prevState.todos)
-            console.log(updatedTodos)
             return {
-                todos: updatedTodos
+                count: prevState.count + 1
             }
         })
     }
     
+    componentDidMount() {
+        console.log("Mounted")
+    }
+    
+    componentDidUpdate() {
+        console.log("Did Update")
+    }
+    
     render() {
-        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
-        
+        console.log("Render")
         return (
-            <div className="todo-list">
-                {todoItems}
+            <div>
+                <h1>{this.state.count}</h1>
+                <button onClick={this.increment}>
+                    Increment!
+                </button>
             </div>
         )
     }
