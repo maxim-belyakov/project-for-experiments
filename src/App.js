cec24864c449446a09f63ef738f24bf5d764f754
Maxim Belyakov <maxim.a.belyakov+g@gmail.com>
2020-03-13 15:31:25 +0200

    show loadinbg

--- src/App.js
+++ src/App.js
@@ -1,52 +1,35 @@
-// https://scrimba.com/g/greacthooks
-// Uncaught Invariant Violation: Maximum update depth exceeded. This can happen when a component repeatedly calls setState inside componentWillUpdate or componentDidUpdate. React limits the number of nested updates to prevent infinite loops.
+import React, {Component} from "react"
+// import Conditional from "./Conditional"
 
-import React from "react"
-import randomcolor from "randomcolor"
-
-class App extends React.Component {
+class App extends Component {
     constructor() {
         super()
         this.state = {
-            count: 0,
-            color: ""
+            isLoading: true
         }
-        this.increment = this.increment.bind(this)
-        this.decrement = this.decrement.bind(this)
     }
     
-    increment() {
-        this.setState(prevState => {
-            return {
-                count: prevState.count + 1
-            }
-        })
-    }
-    decrement() {
-        this.setState(prevState => {
-            return {
-                count: prevState.count - 1
-            }
-        })
-    }
-    
-    componentDidUpdate(prevProps, prevState) {
-        if(prevState.count !== this.state.count) {
-            const newColor = randomcolor()
-            this.setState({color: newColor})
-        }
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                isLoading: false
+            })
+        }, 2000)
     }
     
     render() {
+        let button;
+
+        if (this.state.isLoading) {
+            button = <div>loading</div>
+        } else {
+            button = <div>working</div>
+        }
+
         return (
             <div>
-                <h1 style={{color: this.state.color}}>{this.state.count}</h1>
-                <button onClick={this.increment}>
-                    Increment!
-                </button>
-                <button onClick={this.decrement}>
-                    Decrement!
-                </button>
+                {button}
+                {/* <Conditional isLoading={this.state.isLoading}/> */}
             </div>
         )
     }
