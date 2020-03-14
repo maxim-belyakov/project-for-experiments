cf29e25a621929a20e2e85e93ea441077cbb5ee4
Maxim Belyakov <maxim.a.belyakov+g@gmail.com>
2020-03-14 13:52:54 +0200

    practice lesson

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import React, {Component} from "react"
-// import Conditional from "./Conditional"
+import React from "react"
 
-class App extends Component {
-    constructor() {
+/*
+Challenge:
+
+Given a stateless functional component:
+1. Follow the steps necessary to add state to it,
+2. Have state keep track of whether the user is logged in or not
+3. Add a button that logs the user in/out
+    a. extra challenge - make the button display "log in" if they're not logged in and "log out" if they are
+4. Display text that says "Logged in" if the user is logged in, or "Logged out" if they're not.
+*/
+
+class App extends React.Component {
+    constructor () {
         super()
+        
         this.state = {
-            unreadMessages: ["a", "b"]
+            loginIn: false            
         }
+        
+        this.handleClick = this.handleClick.bind(this)
     }
-    // &&
-    // false && false
-    render() {
+    
+    handleClick () {
+        this.setState(prevState => {
+            return {
+                loginIn: !prevState.loginIn
+            }
+        })
+    }
+    
+    render () {        
+        let login
+        let side
+        
+        if (this.state.loginIn) {
+            login = "Login In"
+            side = "light"
+        } else {
+            login = "Login Out"
+            side = "dark"
+        }
+        
         return (
             <div>
-                {
-                    this.state.unreadMessages.length > 0 && 
-                    <h2>You have {this.state.unreadMessages.length} unread messages!</h2>
-                }
+                <button onClick={this.handleClick}>Come to the {side} side</button>
+                <h1>You are {login}</h1>
             </div>
+
         )
     }
+    
 }
 
 export default App
