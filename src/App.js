f2d61cd5800e0aa2cd151b2889056c0b6683e591
Maxim Belyakov <maxim.a.belyakov+g@gmail.com>
2020-03-17 12:37:35 +0200

    memeGenerator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-// https://medium.com/@dan_abramov/smart-and-dumb-components-7ca2f9a7c7d0
+import React from "react"
 
-import React, {Component} from "react"
-import Form from "./components/FormContainer"
+import Header from "./components/meme/Header"
+import MemeGenerator from "./components/meme/MemeGenerator"
+
+import "./css/meme.css"
+
+/**
+ * Create 2 new components - Header and MemeGenerator
+ * Header will only display things
+ * MemeGenerator will be calling to an API and holding on to data
+ * Each should be in their own file of the same name
+ */
 
 function App() {
     return (
-        <Form />
+        <div>
+            <Header />
+            <MemeGenerator />
+        </div>
     )
 }
 
