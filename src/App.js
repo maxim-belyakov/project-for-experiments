26a9ff1532171db0858401950d81b888a34ca288
Maxim Belyakov <maxim.a.belyakov+g@gmail.com>
2020-03-18 13:24:04 +0200

    insane counting

--- src/App.js
+++ src/App.js
@@ -5,26 +5,17 @@ function App() {
     const [count, setCount] = useState(0)
     const [color, setColor] = useState("")
     
-    function increment() {
-        setCount(prevCount => prevCount + 1)
-    }
-    
-    function decrement() {
-        setCount(prevCount => prevCount - 1)
-    }
-    
     useEffect(() => {
+        // setInterval(() => {
+        //     setCount(prevCount => prevCount + 1)
+        // }, 1000)
+        
         setColor(randomcolor())
-        setInterval(() => {
-            console.log("Interval!")
-        }, 1000)
     }, [count])
     
     return (
         <div>
             <h1 style={{color: color}}>{count}</h1>
-            <button onClick={increment}>Increment</button>
-            <button onClick={decrement}>Decrement</button>
         </div>
     )
 }
