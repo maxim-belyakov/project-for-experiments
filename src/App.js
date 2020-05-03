4721f6f9ae3a92c5e654f361fa2a1588f686c32d
Maxim Belyakov <58391051+maxim-belyakov@users.noreply.github.com>
2020-05-03 18:37:13 +0300

    check that redux is works

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import {connect} from "react-redux"
 
-function App() {
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
-      </header>
+      </header> */}
+      <h1>{props.bananas}</h1>
+      <button>-</button>
+      <button>+</button>
     </div>
   );
 }
 
-export default App;
+function mapStateToProps(globalState) {
+  // return an object where the keys are the name of the prop your component wants,
+  // values are the actual parts of the global state your component wants
+  return {
+      bananas: globalState
+  }
+}
+
+export default connect(mapStateToProps, {})(App)
