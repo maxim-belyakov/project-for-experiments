265a9d325aa85f6bd68bc473e0bacbb5d6f8ee1d
Maxim Belyakov <58391051+maxim-belyakov@users.noreply.github.com>
2020-05-04 21:22:12 +0300

    redux connect

--- src/test5-redux/App.js
+++ src/test5-redux/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import './App.css';
+import React from "react"
+import "./App.css"
 
-function App(props) {
-  return (
-    <div className="App">
-    </div>
-  );
+import {connect} from "react-redux"
+
+function App(props) {    
+    return (
+        <div className="App">
+            <h1>{props.count}</h1>
+            <button>-</button>
+            <button>+</button>
+        </div>
+    )
 }
 
-export default App
+// Write the mapStateToProps function from scratch
+// Takes the global state from Redux as a parameter
+// returns an object where the keys are the name of the prop your component wants,
+// and the values are the actual parts of the global state your component wants
+function mapStateToProps(state) {
+    return {
+        count: state
+    }
+}
+
+
+export default connect(mapStateToProps, {})(App)
