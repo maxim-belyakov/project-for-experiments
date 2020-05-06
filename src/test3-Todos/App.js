8625dabeab41eecae8268baaebc3e30668329852
Maxim Belyakov <58391051+maxim-belyakov@users.noreply.github.com>
2020-05-06 13:44:33 +0300

    recover test 3

--- src/test3-Todos/App.js
+++ src/test3-Todos/App.js
@@ -1,11 +1,68 @@
 import React from 'react';
-import './App.css';
+import './css/App.css';
 
-function App(props) {
-  return (
-    <div className="App">
-    </div>
-  );
+import todosData from "./myJSON/todosData.js"
+import TodoItem from "./TodoItem.js"
+
+class App extends React.Component {
+  constructor() {
+      super()
+      this.state = {
+          todos: todosData,
+          count: 0
+      }
+      this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(id) {
+    // this.setState(prevState => {
+    //   return {
+    //       count: prevState.count + 1
+    //   }
+    // })
+
+    // console.log("I'm working!, count", this.state.count)
+    
+    // this.setState(prevState => {
+    //   const updatedTodos = prevState.todos
+    //   updatedTodos[id - 1].completed = !prevState.todos[id - 1].completed
+    //   return { todos: updatedTodos }
+    // })
+    // console.log(id)
+    // console.log(this.state.todos[id-1])
+
+
+    this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
+        if (todo.id === id) {
+            todo.completed = !todo.completed
+        }
+        return todo
+      })
+      return {
+          todos: updatedTodos
+      }
+    })
+}
+
+  render() {
+    const todoItems = this.state.todos.map(
+      item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>
+    )
+    
+    return (
+      <>
+        <div className="todo-list">
+            {todoItems}
+        </div>
+
+        <div>
+          <h1>{this.state.count}</h1>
+          <button onClick={this.handleClick}>Change!</button>
+        </div>
+      </>
+    )    
+  }
 }
 
 export default App
