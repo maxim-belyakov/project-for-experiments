bb41c135c97ddee0404dfc5bb014d43220e31ee6
Maxim Belyakov <58391051+maxim-belyakov@users.noreply.github.com>
2020-05-06 19:46:45 +0300

    useDispatch hook

--- src/test8-redux-useselector/App.js
+++ src/test8-redux-useselector/App.js
@@ -1,16 +1,18 @@
 import React from "react"
 import "./App.css"
 
-import {useSelector} from "react-redux"
+import {useSelector, useDispatch} from "react-redux"
 import {increment, decrement} from "./redux"
 
 function App(props) {
     const count = useSelector(state => state)
+    const dispatch = useDispatch()
+
     return (
         <div className="App">
             <h1>{count}</h1>
-            <button onClick={props.decrement}>-</button>
-            <button onClick={props.increment}>+</button>
+            <button onClick={() => dispatch(decrement())}>-</button>
+            <button onClick={() => dispatch(increment())}>+</button>
         </div>
     )
 }
