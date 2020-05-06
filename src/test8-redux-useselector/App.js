014317b9b5f1c0d5ac1063e65eb54bd04a515430
Maxim Belyakov <58391051+maxim-belyakov@users.noreply.github.com>
2020-05-06 19:11:00 +0300

    connect store through useSelector

--- src/test8-redux-useselector/App.js
+++ src/test8-redux-useselector/App.js
@@ -1,17 +1,20 @@
 import React from "react"
 import "./App.css"
 
-import {connect} from "react-redux"
+import {useSelector} from "react-redux"
 import {increment, decrement} from "./redux"
 
-function App(props) {    
+function App(props) {
+    const count = useSelector(state => state)
     return (
         <div className="App">
-            <h1>{props.count}</h1>
+            <h1>{count}</h1>
             <button onClick={props.decrement}>-</button>
             <button onClick={props.increment}>+</button>
         </div>
     )
 }
 
-export default connect(state => ({count: state}), {increment, decrement})(App)
+// export default connect(state => ({count: state}), {increment, decrement})(App)
+
+export default App
